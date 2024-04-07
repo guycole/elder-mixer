907c91ea6d76aadbae7a5ac0565cf121a81b2c80
Guy Cole <guycole@gmail.com>
2024-04-07 17:09:52 +0000

    working python client

--- py-client/client.py
+++ py-client/client.py
@@ -8,8 +8,8 @@ import mixer_pb2_grpc
 def execute():
     with grpc.insecure_channel("localhost:50053") as channel:
         stub = mixer_pb2_grpc.MixerStub(channel)
-        response = stub.EnqueueCommand(mixer_pb2.Command(command="play", value=random.randint(0, 100)))
-        print("Response: " + response.message)
+        response = stub.EnqueueCommand(mixer_pb2.EnqueueRequest(command="test", client_id="client"))
+        print("Response: " + response.receipt_id)
 
 if __name__ == "__main__":
     logging.basicConfig()
